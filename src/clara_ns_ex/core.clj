35809747f981fb1c5557d2e46649ab20f1cfb8b0
mpettis_irco <matthew.pettis@irco.com>
2020-01-21 17:31:47 +0000

    Slight more mod, but still broken.

--- src/clara_ns_ex/core.clj
+++ src/clara_ns_ex/core.clj
@@ -10,7 +10,8 @@
 ;;; Queries are in clara-ns-ex.queries
 (let [init-sess (mk-session 'clara-ns-ex.core)
       rules-sess (insert init-sess rule-fail)
-      fact-sess (apply insert rules-sess facts)]
+      query-sess (insert rules-sess get-rulefail)
+      fact-sess (apply insert query-sess facts)]
   (-> fact-sess
       fire-rules
       (query get-rulefail)))
