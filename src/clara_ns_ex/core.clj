2f635eba02411115c8229da2d20da227cbfb1a07
Matt Pettis <matthew.pettis@gmail.com>
2020-01-21 22:42:49 -0600

    Works too, removed a namespace from mk-session.

--- src/clara_ns_ex/core.clj
+++ src/clara_ns_ex/core.clj
@@ -12,7 +12,7 @@
 ;;; Facts are in clara-ns-ex.facts ns
 ;;; Rules are in clara-ns-ex.rules ns
 ;;; Queries are in clara-ns-ex.queries
-(let [init-sess (mk-session 'clara-ns-ex.core 'clara-ns-ex.rules 'clara-ns-ex.queries)
+(let [init-sess (mk-session 'clara-ns-ex.rules 'clara-ns-ex.queries)
       fact-sess (insert-all init-sess facts)]
   (-> fact-sess
       fire-rules
