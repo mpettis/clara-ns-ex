129408b45dd7863cfabff88d4a0fbd664eca76d6
Matt Pettis <matthew.pettis@gmail.com>
2020-01-21 20:02:32 -0600

    This works, but uses no other namespaces.  Everything is in one file.

--- src/clara_ns_ex/core.clj
+++ src/clara_ns_ex/core.clj
@@ -1,14 +1,38 @@
 (ns clara-ns-ex.core
-  (:require [clara.rules :refer :all]
-            [clara-ns-ex.facts :refer :all]
-            [clara-ns-ex.rules :refer :all]
-            [clara-ns-ex.queries :refer :all]))
+  (:require [clara.rules :refer :all]))
 
 
 ;;; Facts are in clara-ns-ex.facts ns
+(defrecord Myfact [theval])
+
+(def facts 
+  [(->Myfact 0)
+   (->Myfact 1)
+   (->Myfact 2)
+   (->Myfact 3)
+   (->Myfact 4)])
+
+;; Rules
+(defrecord Rulefail [theval])
+
+(defrule rule-fail
+  "Rule that checks for failure."
+  [Myfact (= ?theval theval)]
+  [:test (> ?theval 2)]
+  =>
+  (insert! (->Rulefail ?theval)))
+
+
+;; Queries
+(defquery get-rulefail
+  "Get failed rules"
+  []
+  [?rulefail <- Rulefail])
+
+
 ;;; Rules are in clara-ns-ex.rules ns
 ;;; Queries are in clara-ns-ex.queries
-(let [init-sess (mk-session 'clara-ns-ex.core 'clara-ns-ex.rules 'clara-ns-ex.queries)
+(let [init-sess (mk-session 'clara-ns-ex.core)
       fact-sess (insert-all init-sess facts)]
   (-> fact-sess
       fire-rules
